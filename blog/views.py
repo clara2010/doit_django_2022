9ec4fc0d4c807c757b2fd50edc979f4ec76b2e15
clara2010 <okji96@gmail.com>
2022-02-20 23:44:25 +0900

    CVB로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-def index(request):
-    # posts = Post.objects.all() # 모든 post를 다 가져온다.
-    posts = Post.objects.all().order_by('-pk') # 모든 post를 다 가져온다. 역순으로
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts, # 위에서 가져온걸 딕셔너리 형태로 posts라는 이름으로 돌려준다.
-        }
 
-    )
+
+# FBV Style
+# def index(request):
+#     # posts = Post.objects.all() # 모든 post를 다 가져온다.
+#     posts = Post.objects.all().order_by('-pk') # 모든 post를 다 가져온다. 역순으로
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts' : posts, # 위에서 가져온걸 딕셔너리 형태로 posts라는 이름으로 돌려준다.
+#         }
+#
+#     )
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html'
+    # 자동으로 post_list.html 보여줌 따로 지정하고 싶다면 위에 코드드
+
+
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
