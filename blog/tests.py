47c433744e042134bdcbb41a0c16c952ebf5c698
clara2010 <okji96@gmail.com>
2022-03-18 11:18:02 +0900

    검색 기능 구현하기

--- blog/tests.py
+++ blog/tests.py
@@ -473,4 +473,24 @@ class TestView(TestCase):
         self.assertIn('some tag', main_area.text)
         self.assertNotIn('python', main_area.text)
 
+    def test_search(self):
+        # 파이썬이라는 제목의 포스트 생성
+        post_about_python = Post.objects.create(
+            title='파이썬에 대한 포스트 입니다.',
+            content='Hello World. We are the world',
+            author=self.user_trump
+        )
+
+        response = self.client.get('/blog/search/파이썬/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        main_area = soup.find('div', id='main-area')
+
+        # post_003 : 태그, post_about_python : 제목에 파이썬 들어감
+        self.assertIn('Search: 파이썬 (2)', main_area.text)
+        self.assertNotIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertIn(self.post_003.title, main_area.text)
+        self.assertIn(post_about_python.title, main_area.text)
 
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from .models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 # def index(request):
 #     # posts = Post.objects.all() # 모든 post를 다 가져온다.
@@ -209,3 +210,21 @@ def delete_comment(request, pk):
         return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+
+
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
